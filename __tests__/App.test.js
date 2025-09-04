dc005f1d9f58e08532e570768e6e08583fab5c38
D1041221073-IvanaHermili <d1041221073@student.untan.ac.id>
2025-09-04 12:05:10 +0700

    Tugas Latihan Git 1 selesai

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,6 @@
-//import React from 'react';
 import { render } from '@testing-library/react-native';
 import App from '../App';
 
-test('renders welcome text', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('welcome-text')).toBeTruthy();
+test('renders without crashing', () => {
+  render(<App />);
 });
