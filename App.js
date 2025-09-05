7364b97503ab9c0ca5c8fa81a0410f90c2997feb
D1041221073-IvanaHermili <d1041221073@student.untan.ac.id>
2025-09-05 16:33:57 +0700

    Tugas Latihan Git 1 Selesai

--- App.js
+++ App.js
@@ -7,8 +7,6 @@ export default function App() {
       <Text testID="welcome-text">Hello, React Native!</Text>
       <Text style={styles.text}>Nama: Ivana Hermili</Text>
       <Text style={styles.text}>NIM: D1041221073</Text>
-      <Text style={styles.text}>Kelas: A</Text>
-      <Text style={styles.text}>Jenis Kelamin: Perempuan</Text>
     </View>
   );
 }
